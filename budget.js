b365b0d75210b83f49b421b2d5c658717c475173
Lena <olena.mitropolska.gmail.com>
2023-11-06 15:06:08 +0100

    stage two done

--- budget.js
+++ budget.js
@@ -1,7 +1,16 @@
+//stage 1
 let number = document.getElementById("number")
 let n = 0
 number.textContent = n
 
+//stage 2
+let incomeSum = document.getElementById("incomeSum")
+let incomeS = 0
+incomeSum.textContent = incomeS
+
+let expenseSum = document.getElementById("expenseSum")
+let expenseS = 0
+expenseSum.textContent = expenseS
 
 
 const incomeButton = document.getElementById("incomeButton")
@@ -10,18 +19,23 @@ const expenseButton = document.getElementById("expenseButton")
 
 incomeButton.addEventListener("click", function () {
 const incomeInput = document.getElementById("incomeInput")
-console.log(incomeInput.value);
+// console.log(incomeInput.value);
 n += Number(incomeInput.value)
 number.textContent = n
-incomeInput.value = ""
+incomeS += Number(incomeInput.value)
+incomeSum.textContent = incomeS
 
+incomeInput.value = ""
 })
 
 expenseButton.addEventListener("click", function ()  {
     const expenseInput = document.getElementById("expenseInput")
-    console.log(expenseInput.value);
+    // console.log(expenseInput.value);
     n -= Number(expenseInput.value)
     number.textContent = n
+    expenseS += Number(expenseInput.value)
+    expenseSum.textContent = expenseS
+
     expenseInput.value = ""
 })
 
